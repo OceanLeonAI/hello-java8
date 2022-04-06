bcbdd9f98514f19a99a3ead07b13bbef8639ddcf
OceanLeonAI <beyondjayfir@126.com>
2022-04-07 00:07:14 +0800

    initial the repository

---
+++ src/test/java/com/leon/hello/java8/HelloJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.hello.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJava8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
