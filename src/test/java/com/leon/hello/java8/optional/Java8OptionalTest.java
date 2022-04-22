c1e80bc6c2dd75dd4c1b29d7d69e27ec734217d2
OceanLeonAI <beyondjayfir@126.com>
2022-04-22 21:32:31 +0800

    update demo

---
+++ src/test/java/com/leon/hello/java8/optional/Java8OptionalTest.java
@@ -0,0 +1,12 @@
+package com.leon.hello.java8.optional;
+
+/**
+ * @PROJECT_NAME: hello-java8
+ * @CLASS_NAME: Java8OptionalTest
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2022/4/22 18:23
+ * @Version 1.0
+ * @DESCRIPTION: Optional测试
+ **/
+public class Java8OptionalTest {
+}
--- src/test/java/com/leon/hello/java8/stream/Java8StreamTest.java
+++ src/test/java/com/leon/hello/java8/stream/Java8StreamTest.java
@@ -169,11 +169,169 @@ public class Java8StreamTest {
     public void flatMap2() {
     }
 
-
     // ==================== 中间操作 end ====================
 
     // ==================== 终结操作 begin ====================
 
+    /**
+     * forEach 对流中的元素进行遍历操作，我们通过传入的参数去指定对遍历到的元素进行什么具体操作。
+     *
+     * <p>
+     * 输出所有作家的名字
+     */
+    @Test
+    public void forEach() {
+    }
+
+    /**
+     * count 可以用来获取当前流中元素的个数。
+     *
+     * <p>
+     * 打印这些作家的所出书籍的数目，注意删除重复元素。
+     */
+    @Test
+    public void count() {
+    }
+
+    /**
+     * max&min 可以用来或者流中的最值。
+     *
+     * <p>
+     * 分别获取这些作家的所出书籍的最高分和最低分并打印。
+     */
+    @Test
+    public void maxOrMin() {
+    }
+
+    /**
+     * collect 把当前流转换成一个集合。
+     *
+     * <p>
+     * 1.获取一个存放所有作者名字的List集合。
+     * 2.获取一个所有书名的Set集合。
+     * 3.获取一个Map集合，map的key为作者名，value为List
+     */
+    @Test
+    public void collect() {
+    }
+
     // ==================== 终结操作 end ====================
+
+
+    // ==================== 查找与匹配 begin ====================
+
+    /**
+     * anyMatch 可以用来判断是否有任意符合匹配条件的元素，结果为boolean类型。
+     *
+     * <p>
+     * 判断是否有年龄在29以上的作家
+     */
+    @Test
+    public void anyMatch() {
+    }
+
+    /**
+     * allMatch 可以用来判断是否都符合匹配条件，结果为boolean类型。如果都符合结果为true，否则结果为false。
+     *
+     * <p>
+     * 判断是否有年龄在29以上的作家
+     */
+    @Test
+    public void allMatch() {
+    }
+
+    /**
+     * noneMatch 可以判断流中的元素是否都不符合匹配条件。如果都不符合结果为true，否则结果为false
+     *
+     * <p>
+     * 判断作家是否都没有超过100岁的。
+     */
+    @Test
+    public void noneMatch() {
+    }
+
+    /**
+     * findAny 获取流中的任意一个元素。该方法没有办法保证获取的一定是流中的第一个元素。
+     *
+     * <p>
+     * 获取任意一个年龄大于18的作家，如果存在就输出他的名字
+     */
+    @Test
+    public void findAny() {
+    }
+
+    /**
+     * findFirst 获取流中的第一个元素。
+     *
+     * <p>
+     * 获取一个年龄最小的作家，并输出他的姓名。
+     */
+    @Test
+    public void findFirst() {
+    }
+
+    // ==================== reduce归并(缩减操作) begin ====================
+
+    /**
+     *对流中的数据按照你指定的计算方式计算出一个结果。（缩减操作）
+     * reduce的作用是把stream中的元素给组合起来，我们可以传入一个初始值，它会按照我们的计算方
+     * 式依次拿流中的元素和初始化值进行计算，计算结果再和后面的元素计算。
+     * reduce两个参数的重载形式内部的计算方式如下
+     * T result = identity;
+     * for (T element : this stream)
+     *      result = accumulator.apply(result, element)
+     * return result;
+     *
+     * 其中identity就是我们可以通过方法参数传入的初始值，accumulator的apply具体进行什么计算也是
+     * 我们通过方法参数来确定的。
+     */
+
+    /**
+     * 使用reduce求所有作者年龄的和
+     */
+    @Test
+    public void reduce1() {
+    }
+
+    /**
+     * 使用reduce求所有作者中年龄的最大值
+     */
+    @Test
+    public void reduce2() {
+    }
+
+    /**
+     * 使用reduce求所有作者中年龄的最小值
+     */
+    @Test
+    public void reduce3() {
+
+    }
+
+    /**
+     * reduce一个参数的重载形式内部的计算
+     *         boolean foundAny = false;
+     *         T result = null;
+     *         for (T element : this stream) {
+     *             if (!foundAny) {
+     *                 foundAny = true;
+     *                 result = element;
+     *             } e
+     *                     lse
+     *             result = accumulator.apply(result, element);
+     *         } r
+     *         eturn foundAny ? Optional.of(result) : Optional.empty();
+     *
+     */
+
+    /**
+     * 如果用一个参数的重载方法去求最小值
+     */
+    @Test
+    public void reduce4() {
+
+    }
+
+    // ==================== reduce归并(缩减操作) end ====================
 
 }
