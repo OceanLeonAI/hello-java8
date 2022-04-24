a9c5b40a0fc3a9b903d183aaadbf5800f8911333
OceanLeonAI <beyondjayfir@126.com>
2022-04-24 16:47:49 +0800

    update demo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hello-java8
+
 学习 java8 特性
+
+## Lambda
+
+## Stream
+
+## Opetional
+
+## Functional
+
+## Method Reference
+
+## Advanced Feature
--- src/test/java/com/leon/hello/java8/methodreference/MethodReferenceTest.java
+++ src/test/java/com/leon/hello/java8/methodreference/MethodReferenceTest.java
@@ -1,5 +1,13 @@
 package com.leon.hello.java8.methodreference;
 
+import com.leon.hello.java8.entity.Author;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.leon.hello.java8.entity.Author.getAuthors;
+
 /**
  * @PROJECT_NAME: hello-java8
  * @CLASS_NAME: MethodReferenceTest
@@ -12,4 +20,88 @@ package com.leon.hello.java8.methodreference;
  * 用进一步简化代码。
  **/
 public class MethodReferenceTest {
+
+    /**
+     * 引用类的静态方法
+     * 类名::方法名
+     */
+    @Test
+    public void classStaticMethodReferenceTest() {
+        List<Author> authors = getAuthors();
+        Stream<Author> authorStream = authors.stream();
+
+        authorStream.map(author -> author.getAge())
+                .map(age -> String.valueOf(age));
+
+        // lambda
+        // authorStream.map(Author::getAge)
+        // .map(String::valueOf);
+    }
+
+    /**
+     * 引用对象的实例方法
+     * 对象名::方法名
+     */
+    @Test
+    public void objectInstantiateMethodReferenceTest() {
+        List<Author> authors = getAuthors();
+        Stream<Author> authorStream = authors.stream();
+        StringBuilder sb = new StringBuilder();
+        authorStream.map(author -> author.getName())
+                .forEach(name -> sb.append(name));
+        // 优化后
+        authorStream.map(Author::getName)
+                .forEach(sb::append);
+    }
+
+
+    /**
+     * 引用对象的实例方法
+     * 对象名::方法名
+     */
+
+    interface UseString {
+        String use(String str, int start, int length);
+    }
+
+    public static String subAuthorName(String str, UseString useString) {
+        int start = 0;
+        int length = 1;
+        return useString.use(str, start, length);
+    }
+
+    @Test
+    public void classInstantiateMethodReferenceTest() {
+
+        subAuthorName("三更草堂", new UseString() {
+            @Override
+            public String use(String str, int start, int length) {
+                return str.substring(start, length);
+            }
+        });
+
+        // 优化后
+        subAuthorName("三更草堂", String::substring);
+    }
+
+    /**
+     * 构造器引用
+     * 类名::new
+     */
+    @Test
+    public void constructorMethodReferenceTest() {
+        List<Author> authors = getAuthors();
+        authors.stream()
+                .map(author -> author.getName())
+                .map(name->new StringBuilder(name))
+                .map(sb->sb.append("-三更").toString())
+                .forEach(str-> System.out.println(str));
+        // 优化后
+        authors.stream()
+                .map(Author::getName)
+                .map(StringBuilder::new)
+                .map(sb->sb.append("-三更").toString())
+                .forEach(System.out::println);
+    }
+
 }
